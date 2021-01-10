8ab05703c75d671fbe17cb36c5a00c6e7715930e
Saurabh <emosrb@gmail.com>
2021-01-10 13:24:52 +0530

    did data binding/Interpolation for username and password from login.component.ts to login.component.html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'surbhi'
+  password = ''
   constructor() { }
 
   ngOnInit(): void {
