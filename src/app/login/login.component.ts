e9772b359a7bb4a3e47e987514cf84fb9a63703b
Saurabh <emosrb@gmail.com>
2021-01-11 09:43:17 +0530

    Implemented Routing from Login to Welcome Component using Angular Dependency Injection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   password = ''
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -19,6 +20,8 @@ export class LoginComponent implements OnInit {
   HandleLogin(){
 
     if(this.username === "surbhi" && this.password === 'dummy'){
+      //Redirect to Welcome Page
+      this.router.navigate(['welcome'])
       this.invalidLogin = false
     }else{
       this.invalidLogin = true
