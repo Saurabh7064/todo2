14ed2296a046bb2edb17f8ef774ed87d97c2aa5d
Saurabh <emosrb@gmail.com>
2021-01-12 09:23:06 +0530

    Addedd new class in ListTodoComponent to create todo list using constructor and used pipeline to change date format to just Date <td>{{todo.targetDate | date | uppercase }}</td>

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+
+    constructor(
+      public id: number,
+      public description: string,
+      public done: boolean,
+      public targetDate: Date
+    ){
+
+    }
+
+}
+
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,10 +21,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
  todos = [
-      {id:1,description:'learn to dance'},
-      {id:2,description:'have food'},
-      {id:3,description:'learn to play guitar'},
-      {id:4,description:'buy grocery'}
+      new Todo(1,'learn to dance',false,new Date()),
+      new Todo(2,'have food',false,new Date()),
+      new Todo(3,'learn to play guitar',false,new Date()),
+      new Todo(4,'buy grocery',false,new Date())
 
  ]
 
