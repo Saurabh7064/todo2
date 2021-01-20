f0cf1ef35c8b2d31152e7cf95502b63e75cb08c2
Saurabh <emosrb@gmail.com>
2021-01-20 09:52:08 +0530

    Creating Retrive Todo Service and connect to fornt end

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Todo } from '../list-todos/list-todos.component';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  id:number
+  todo: Todo
 
-  ngOnInit(): void {
+  constructor(
+    private todoService:TodoDataService,
+    private route:ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.todo = new Todo(1,'',false,new Date())
+    this.todoService.retrieveTodo('in28minutes',this.id).subscribe(
+      data => this.todo = data
+    )
   }
 
 }
