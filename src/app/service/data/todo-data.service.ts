3477892935a1f24ab6bd918d47c46ffe0421d90c
saurabh <emosrb@gmail.com>
2021-01-23 15:07:05 +0530

    implementing update todo operation in frontend

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -12,15 +12,20 @@ export class TodoDataService {
   ) { }
 
   retrieveAllTodos(username){
-    return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`);
+    return this.http.get<Todo[]>(`http://localhost:8092/users/${username}/todos`);
     //console.log("execute hellow world bean");
   }
 
   deleteTodo(username,id){
-    return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
+    return this.http.delete(`http://localhost:8092/users/${username}/todos/${id}`);
   }
 
   retrieveTodo(username,id){
-    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+    return this.http.get<Todo>(`http://localhost:8092/users/${username}/todos/${id}`);
   }
+
+  updateTodo(username, id, todo){
+    return this.http.put(`http://localhost:8092/users/${username}/todos/${id}`,todo);
+  }
+
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -26,4 +26,12 @@ export class TodoComponent implements OnInit {
     )
   }
 
+  saveTodo(){
+    this.todoService.updateTodo('in28minutes',this.id,this.todo).subscribe(
+      data =>{ 
+        console.log(data)
+      }
+    )
+  }
+
 }
