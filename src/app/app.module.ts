8dcc4ea0b826a0414d009b956b557178fd28313d
saurabh <emosrb@gmail.com>
2021-01-23 18:03:49 +0530

    Configuring HttpInterceptor as Provider in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { ListTodosComponent } from './list-todos/list-todos.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { LogoutComponent } from './logout/logout.component';
-import {  HttpClientModule } from '@angular/common/http';
+import {  HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpInterceptorBasicAuthService } from './service/http/http-interceptor-basic-auth.service';
 
 
 @NgModule({
@@ -32,7 +33,7 @@ import {  HttpClientModule } from '@angular/common/http';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide : HTTP_INTERCEPTORS, useClass: HttpInterceptorBasicAuthService, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
