aba8ff6804ec2a7ec50b66ecd3cdad58fbf6e542
saurabh <emosrb@gmail.com>
2021-01-23 21:54:29 +0530

    connected login page to basic Authentication Service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HardcodedAuthenticationService } from './../service/hardcoded-authentication.service';
+import { BasicAuthenticationService } from './../service/basic-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,8 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
   constructor(private router:Router,
-    private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+    private hardcodedAuthenticationService: HardcodedAuthenticationService,
+    private basicAuthenticationService:BasicAuthenticationService) { }
 
   ngOnInit(): void {
   }
@@ -32,5 +34,21 @@ export class LoginComponent implements OnInit {
     // console.log(this.username);
     // console.log(this.password);
   }
+
+
+  HandleBasicAuthLogin(){
+     this.basicAuthenticationService.executeAuthenticationService(this.username ,this.password ).subscribe(
+       data => {
+         console.log(data)
+         this.router.navigate(['welcome',this.username])
+         this.invalidLogin = false
+       },
+       error =>{
+        console.log(error)
+        this.invalidLogin = true
+       }
+     )
+      
+   }
 
 }
---
+++ src/app/service/basic-authentication.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {map} from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  executeAuthenticationService(username, password){
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    
+    
+    let headers = new HttpHeaders({
+      Authorization : basicAuthHeaderString
+    })
+
+    return this.http.get<AuthenticationBean>(`http://localhost:8092/basicauth`,
+    {headers}).pipe(
+      map(
+      data => {
+        sessionStorage.setItem('authenticatedUser',username);
+        return data;
+      }
+      )
+    );
+    //console.log("execute hellow world bean");
+  } 
+
+
+  authenticate(username, password){
+    console.log('before'+this.isUserLoggedIn());
+    if(username === "surbhi" && password === 'dummy'){
+      sessionStorage.setItem('authenticatedUser',username);
+      console.log('after'+this.isUserLoggedIn());
+            return true;
+    }
+      return false;
+    
+  }
+
+  isUserLoggedIn(){
+   let user= sessionStorage.getItem('authenticatedUser');
+   return !(user === null)
+  }
+
+  logout(){
+    sessionStorage.removeItem('authenticatedUser');
+  }
+}
+
+export class AuthenticationBean{
+  constructor(public message:String){}
+}
